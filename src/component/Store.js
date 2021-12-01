3f92e0f91642a31017f0250ec3d3bb2021d44866
Gabriel <gabrielcpf2997@gmail.com>
2021-11-30 22:57:28 -0800

    added item in store pages

--- src/component/Store.js
+++ src/component/Store.js
@@ -1,9 +1,37 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
+import uniqid from "uniqid";
+import getImgaes from "../images/getImages";
 
 const Store = () => {
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {
+        const imgArr = getImgaes();
+        const itemArr = [];
+        for (let i = 0; i < imgArr.length; i++) {
+            itemArr.push({
+                id: uniqid(),
+                type: imgArr[i].type,
+                img: imgArr[i].img,
+                name: imgArr[i].name
+            })
+        }
+        setItems(itemArr);
+    }, []);
+
     return (
         <div className="store">
-            <h1>this is Store</h1>
+            <h1>store</h1>
+            <ul className="items">
+                {items.map(item => {
+                    return (
+                        <li className="item" key={item.id}>
+                            <img src={item.img} alt={item.name}/>
+                            <label>{item.name}</label>
+                        </li>
+                    )
+                })}
+            </ul>
         </div>
     );
 }
---
+++ src/images/getImages.js
@@ -0,0 +1,37 @@
+import img1 from "./Awa_Awa_no_Mi_Infobox.png";
+import img2 from "./Doru_Doru_no_Mi_Infobox.png";
+import img3 from "./Gomu_Gomu_no_Mi_Infobox.png";
+import img4 from "./Hana_Hana_no_Mi_Infobox.png";
+import img5 from "./Hito_Hito_no_Mi_Infobox.png";
+import img6 from "./Mera_Mera_no_Mi_Infobox.png";
+import img7 from "./Moku_Moku_no_Mi_Infobox.png";
+import img8 from "./Ope_Ope_no_Mi_Infobox.png";
+import img9 from "./Ushi_Ushi_no_Mi,_Model_Giraffe_Infobox.png";
+import img10 from "./Yami_Yami_no_Mi_Infobox.png";
+
+function getImgaes() {
+    const imgArr = [];
+
+    imgArr.push(_imgItem(img1, 'paramecia', 'awa awa'));
+    imgArr.push(_imgItem(img2, 'paramecia', 'doru doru'));
+    imgArr.push(_imgItem(img3, 'paramecia', 'gomu gomu'));
+    imgArr.push(_imgItem(img4, 'paramecia', 'hana hana'));
+    imgArr.push(_imgItem(img5, 'Zoan', 'hito hito'));
+    imgArr.push(_imgItem(img6, 'logia', 'mera mera'));
+    imgArr.push(_imgItem(img7, 'logia', 'moku moku'));
+    imgArr.push(_imgItem(img8, 'paramecia', 'ope ope'));
+    imgArr.push(_imgItem(img9, 'Zoan', 'ushi ushi(giraffe)'));
+    imgArr.push(_imgItem(img10, 'logia', 'yami yami'));
+
+    return imgArr;
+}
+
+function _imgItem(src, theType, theName) {
+    return ({
+        img: src,
+        type: theType,
+        name: theName
+    });
+}
+
+export default getImgaes;
